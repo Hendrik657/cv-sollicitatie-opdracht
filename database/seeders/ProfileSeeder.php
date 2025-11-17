532f6b81b09db00d792c57ccf2b56ffdb2f2bbbc
Hendrik657 <204034832+Hendrik657@users.noreply.github.com>
2025-11-17 18:23:59 +0100

    leerjaar bijgewerkt

--- database/seeders/ProfileSeeder.php
+++ database/seeders/ProfileSeeder.php
@@ -14,7 +14,7 @@ class ProfileSeeder extends Seeder
     public function run(): void
     {
         Profile::create([
-			'bio' => 'Hallo, ik ben Hendrik, een 20-jarige student van het Koning Willem 1 College in Den Bosch. Momenteel studeer ik Software Developer en zit in leerjaar 2. Naast mijn opleiding werk ik ook als verkoopmedewerker bij Hoogvliet Supermarkt. In mijn vrije tijd ben ik bezig met gamen, schoolwerk en zelf programmeren.',
+			'bio' => 'Hallo, ik ben Hendrik, een 20-jarige student van het Koning Willem 1 College in Den Bosch. Momenteel studeer ik Software Developer en zit in het examen jaar. Naast mijn opleiding werk ik ook als verkoopmedewerker bij Hoogvliet Supermarkt. In mijn vrije tijd ben ik bezig met gamen, schoolwerk en zelf programmeren.',
 			'skills' => 'Zelfstandig, Zorgvuldig, Harde werker, Nieuwsgierig, Verantwoordelijk',
 			'technical_skills' => 'HTML, CSS, JavaScript, PHP, C#, MySQL, MSSQL, Vue.js, Node.js, Bootstrap, Less.js, Laravel, Docker, Tailwind',
 			'work_experience' => 'Hoogvliet Supermarkt, Spotta',
